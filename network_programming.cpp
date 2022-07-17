63b6554771d0480c05712cb19280d383b27418a8
Phillip Schackier <CppPhil@users.noreply.github.com>
2022-07-17 14:00:48 +0200

    Initial Commit

---
+++ network_programming.cpp
@@ -0,0 +1,12 @@
+﻿// network_programming.cpp : Defines the entry point for the application.
+//
+
+#include "network_programming.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
